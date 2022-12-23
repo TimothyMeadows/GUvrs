9e0a3a617cbd7005794bbaa16d04d542e33174eb
Timothy D Meadows II <birdgineer@Timothys-MacBook-Air.local>
2022-12-22 23:06:15 -0800

    add label bindings

--- GUvrs/MainPage.xaml.cs
+++ GUvrs/MainPage.xaml.cs
@@ -2,16 +2,50 @@
 
 public partial class MainPage : ContentPage
 {
+	private long PlayerId = 0;
+	private long OpponentId = 0;
+
 	public MainPage()
 	{
 		InitializeComponent();
-	}
+
+        PlayerID.GestureRecognizers.Add(new ClickGestureRecognizer()
+		{
+			Command = new Command(() => OnIDClick(PlayerId)),
+			NumberOfClicksRequired = 1
+		});
+
+        PlayerID.GestureRecognizers.Add(new TapGestureRecognizer()
+        {
+            Command = new Command(() => OnIDClick(PlayerId))
+        });
+
+        OpponentID.GestureRecognizers.Add(new ClickGestureRecognizer()
+        {
+            Command = new Command(() => OnIDClick(OpponentId)),
+            NumberOfClicksRequired = 1
+        });
+
+        OpponentID.GestureRecognizers.Add(new TapGestureRecognizer()
+        {
+            Command = new Command(() => OnIDClick(OpponentId))
+        });
+    }
 
 	private void OnResetClick(object sender, EventArgs e)
 	{
 		Reset.IsEnabled = false;
 		// Do Work
 		Reset.IsEnabled = true;
+	}
+
+	private void OnIDClick(long id)
+	{
+		if (id == 0 || id == -1)
+			return;
+
+		//Browser.OpenAsync("https://www.gudecks.com/");
+		return;
 	}
 }
 
