2f9b88c9ee1dacdf56f96953c6eda09b58516d79
Timothy D Meadows II <timothy@timothymeadows.com>
2022-12-23 00:02:02 -0800

    add CrossPlatform, and GuLogPath for Mac, and Windows

--- GUvrs/Startup.cs
+++ GUvrs/Startup.cs
@@ -1,16 +1,20 @@
 ﻿using System;
+using GUvrs.Modules;
+
 namespace GUvrs
 {
 	public static class Startup
 	{
 		public static void MacCatalyst()
 		{
-			return;
+			CrossPlatform.GuLogPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Library",
+                "Logs", "Immutable", "gods");
 		}
 
 		public static void Windows()
 		{
-			return;
+            CrossPlatform.GuLogPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "AppData",
+                "LocalLow", "Immutable", "gods");
 		}
 	}
 }
